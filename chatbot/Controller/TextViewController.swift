5974bc7cb5234d831699cf217b684e3f9fd207cc
Swamita <swamita2001@gmail.com>
2020-10-11 08:15:08 +0530

    Add speech to text

--- chatbot/Controller/TextViewController.swift
+++ chatbot/Controller/TextViewController.swift
@@ -7,8 +7,15 @@
 
 import UIKit
 import AWSLex
+import Speech
 
-class TextViewController: UIViewController, AWSLexInteractionDelegate, UITextFieldDelegate {
+class TextViewController: UIViewController, AWSLexInteractionDelegate, UITextFieldDelegate, SFSpeechRecognizerDelegate  {
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))
+    
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
     
     var interactionKit: AWSLexInteractionKit?
     
@@ -75,6 +82,34 @@ class TextViewController: UIViewController, AWSLexInteractionDelegate, UITextFie
         tableView.register(UINib(nibName: "BotCell", bundle: nil), forCellReuseIdentifier: "botCell")
         textField.delegate = self
         setUpLex()
+        speechRecognizer!.delegate = self
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+                
+            var isButtonEnabled = false
+                
+            switch authStatus {
+                case .authorized:
+                    isButtonEnabled = true
+                    
+                case .denied:
+                    isButtonEnabled = false
+                    print("User denied access to speech recognition")
+                    
+                case .restricted:
+                    isButtonEnabled = false
+                    print("Speech recognition restricted on this device")
+                    
+                case .notDetermined:
+                    isButtonEnabled = false
+                    print("Speech recognition not yet authorized")
+            @unknown default:
+                fatalError("SF Speech recognition failed!")
+            }
+                
+            OperationQueue.main.addOperation() {
+                    self.micButton.isEnabled = isButtonEnabled
+                }
+        }
         /*NotificationCenter.default.addObserver(self, selector: #selector(TextViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(TextViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)*/
         }
@@ -92,6 +127,95 @@ class TextViewController: UIViewController, AWSLexInteractionDelegate, UITextFie
             self.view.frame.origin.y += keyboardSize.height
         }
     }*/
+    @IBAction func micTapped(_ sender: UIButton) {
+        if audioEngine.isRunning {
+                audioEngine.stop()
+                recognitionRequest?.endAudio()
+                micButton.isEnabled = false
+            
+            } else {
+                startRecording()
+            }
+    }
+    @IBAction func searchPressed(_ sender: Any) {
+        if textField.text == "Say something, I'm listening!" {
+            textField.text = "Speak on the microphone to record your request..."
+        } else {
+            sendMessage(text: textField.text!)
+        }
+    }
+    
+    //MARK: - Mic and speech Functionalities
+    
+    func startRecording() {
+        
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSession.Category.record)
+            try audioSession.setMode(AVAudioSession.Mode.measurement)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        let inputNode = audioEngine.inputNode
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer!.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            
+            var isFinal = false
+            
+            if result != nil {
+                
+                self.textField.text = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.micButton.isEnabled = true
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            print("audioEngine couldn't start because of an error.")
+        }
+        textField.text = "Say something, I'm listening!"
+        
+    }
+    
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            micButton.isEnabled = true
+        } else {
+            micButton.isEnabled = false
+        }
+    }
     
 }
 
@@ -114,6 +238,4 @@ extension TextViewController: UITableViewDelegate, UITableViewDataSource {
     }
 }
 
-/*
- [OrderedDict([('id', '12'), ('season', '2017'), ('city', 'Bangalore'), ('date', '2017-04-14'), ('team1', 'Royal Challengers Bangalore'), ('team2', 'Mumbai Indians'), ('toss_winner', 'Mumbai Indians'), ('toss_decision', 'field'), ('result', 'normal'), ('dl_applied', '0'), ('winner', 'Mumbai Indians'), ('win_by_runs', '0'), ('win_by_wickets', '4'), ('player_of_match', 'KA Pollard'), ('venue', 'M Chinnaswamy Stadium'), ('umpire1', 'KN Ananthapadmanabhan'), ('umpire2', 'AK Chaudhary'), ('umpire3', '')]), OrderedDict([('id', '37'), ('season', '2017'), ('city', 'Mumbai'), ('date', '2017-05-01'), ('team1', 'Royal Challengers Bangalore'), ('team2', 'Mumbai Indians'), ('toss_winner', 'Royal Challengers Bangalore'), ('toss_decision', 'bat'), ('result', 'normal'), ('dl_applied', '0'), ('winner', 'Mumbai Indians'), ('win_by_runs', '0'), ('win_by_wickets', '5'), ('player_of_match', 'RG Sharma'), ('venue', 'Wankhede Stadium'), ('umpire1', 'AK Chaudhary'), ('umpire2', 'CB Gaffaney'), ('umpire3', '')])]
- */
+
