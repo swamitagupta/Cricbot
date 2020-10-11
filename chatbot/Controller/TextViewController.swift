0b59e4f161a3c361c46852b6b2eb6cf767939128
Swamita <swamita2001@gmail.com>
2020-10-11 10:10:53 +0530

    Keyboard Fix

--- chatbot/Controller/TextViewController.swift
+++ chatbot/Controller/TextViewController.swift
@@ -78,7 +78,8 @@ class TextViewController: UIViewController, AWSLexInteractionDelegate, UITextFie
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        NotificationCenter.default.addObserver(self, selector: #selector(TextViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(TextViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(UINib(nibName: "UserCell", bundle: nil), forCellReuseIdentifier: "userCell")
@@ -116,29 +117,16 @@ class TextViewController: UIViewController, AWSLexInteractionDelegate, UITextFie
         
         }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-
-            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-            NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    @objc func keyboardWillHide() {
-        self.view.frame.origin.y = -10
-    }
-
-    @objc func keyboardWillChange(notification: NSNotification) {
-
+    @objc func keyboardWillShow(_ notification:Notification) {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-            if textField.isFirstResponder {
-                self.view.frame.origin.y = -keyboardSize.height
-            }
+            self.view.frame.origin.y -= keyboardSize.height
+        }
+    }
+
+    @objc func keyboardWillHide(_ notification:Notification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            self.view.frame.origin.y += keyboardSize.height
         }
     }
     
